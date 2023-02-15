4b6b735bf563a17ecd759f288de3ead384ead21e
jayy-77 <1427jay@gmail.com>
2023-02-15 21:12:43 +0530

    V1.4 => Features - Showing split data

--- Room.py
+++ Room.py
@@ -6,14 +6,14 @@ def room_init(room_id,response):
         doc_ref_room = db.collection("Rooms").document(room_id)
         doc_room_data = doc_ref_room.get().to_dict()
         print("Room Name:",doc_room_data['room_name'])
-        print("_____Room Data_______")
-        show_split_data()
         print("1.Split Request\n2.Split Info\n")
         choice = None
         while choice != 'q':
             choice = int(input("choice: "))
             if choice == 1:
                 split_request(room_id,response,doc_ref_room)
+            elif choice == 2:
+                show_split_data(room_id)
 def split_request(room_id,response,doc_ref_room):
     user_dict = {}
     split_dict = {}
@@ -45,8 +45,21 @@ def split_request(room_id,response,doc_ref_room):
     }
     doc_ref_room.collection("Splits").document(str(random.randint(1000,9999))).set(split_dict)
     print("Split created successfully.")
-def show_split_data():
-    pass
+def show_split_data(room_id):
+    room_data = {}
+    doc_split_ref = db.collection("Rooms").document(room_id).collection("Splits")
+    split_codes = [doc.id for doc in doc_split_ref.stream()]
+    for i in split_codes:
+        room_data_dict = doc_split_ref.document(i).get().to_dict()
+        room_data[i] = room_data_dict
+    for key,value in room_data.items():
+        split_head = PrettyTable(['Code: '+key,'Split Name: '+value['split_name']])
+        split_head.add_row(['Split Sender: ',value['split_sender']])
+        split_head.add_row(['Amount: ',value['split_amount']])
+        split_head.add_row(['Total Amount: ',value['total_amount']])
+        split_head.add_row(['Paid: ',value['paid']])
+        split_head.add_row(['UnPaid: ',value['unpaid']])
+        print(split_head)
 def user_info(room_id):
     doc_ref_user = db.collection("Rooms").document(room_id)
     return doc_ref_user.get().to_dict()['joined_users']
